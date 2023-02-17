7a5f02a409e50f00a68b40f339802e7f921d27fb
Latoya Wenzinger <lnorman1016@gmail.com>
2023-02-16 23:59:41 -0500

    added ERD image and updated schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,59 +1,64 @@
+-- create table schemas
 CREATE TABLE departments (
     dept_no VARCHAR(30) NOT NULL,
     dept_name VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_departments PRIMARY KEY dept_no
-     
+	 PRIMARY KEY (dept_no)
 );
 
-CREATE TABLE dept_emp (
-    emp_no INT NOT NULL,
-    dept_no VARCHAR(30) NOT NULL,
-    CONSTRAINT pk_dept_emp PRIMARY KEY emp_no
-     
-);
-
-CREATE TABLE dept_manager (
-    dept_no VARCHAR(30) NOT NULL,
-    emp_no INT NOT NULL
-);
-
-CREATE TABLE employees (
+CREATE TABLE employees(
     emp_no INT NOT NULL,
     emp_title VARCHAR(30) NOT NULL,
     birth_date DATE NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     sex VARCHAR(1) NOT NULL,
-    hire_date DATE NOT NULL
+    hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE salaries (
-    emp_no INTEGER   NOT NULL,
-    salary INTEGER   NOT NULL
+CREATE TABLE dept_manager(
+    dept_no VARCHAR(30) NOT NULL,
+    emp_no INT NOT NULL,
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
 
-CREATE TABLE titles (
-    title_id VARCHAR(30)   NOT NULL,
-    title VARCHAR(50)   NOT NULL,
-    CONSTRAINT pk_titles PRIMARY KEY title_id
-     
+CREATE TABLE dept_emp(
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(30) NOT NULL,
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
+CREATE TABLE salaries(
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
+CREATE TABLE titles(
+    title_id VARCHAR(30) NOT NULL,
+    title VARCHAR(50) NOT NULL
+);
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "dept_emp" ("emp_no");
+-- List the employee number, last name, first name, sex, and salary of each employee.
+SELECT salaries.emp_no, salaries.salary, employees.last_name, employees.first_name, employees.sex
+FROM salaries
+INNER JOIN employees ON
+employees.emp_no=salaries.emp_no;
 
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "dept_emp" ("emp_no");
+-- List the first name, last name, and hire date for the employees who were hired in 1986
+SELECT first_name, last_name, hire_date
+FROM  employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
 
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
-REFERENCES "titles" ("title_id");
 
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "dept_emp" ("emp_no");
+
+
+  
+
+
+
+
+
 
